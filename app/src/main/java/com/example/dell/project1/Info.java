6d400d43c67065b50a399ac624a5c1a506e38d37
Prakash Dwivedi <Prakash Dwivedi>
2017-03-29 01:13:34 +0530

    Initial commit

---
+++ app/src/main/java/com/example/dell/project1/Info.java
@@ -0,0 +1,20 @@
+package com.example.dell.project1;
+
+
+public class Info {
+    char[] alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+
+    public static String[] progNames = new String[26] ;
+    public int[] quantity = new int[26];
+    public int[] prices = new int[26];
+
+    void NamingFn()
+    {
+        for(int i=0;i<26;++i)
+        {
+            progNames[i] = Character.toString(alphabet[i]);
+            quantity[i] = 0 ;
+            prices[i] = (int)(Math.random()*100+1) ;
+        }
+    }
+}
